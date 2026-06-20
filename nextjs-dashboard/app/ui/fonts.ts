67cb5b9e9a5717afde45b851cd532128f21ac1b7
Ivan Aranda <iaranda264@gmail.com>
2026-06-20 16:54:18 -0300

    first commit

---
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -0,0 +1,5 @@
+//este archivo guardara las fuentes que se utilizaran en toda la aplicacion
+import {Inter} from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+export const lusitana = Lusitana({weight:["400","700"],subsets:['latin']});
+export const inter = Inter({subsets:['latin']});
